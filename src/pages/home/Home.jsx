576946287133993a91926a60b992dcc298eca5bd
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-21 10:08:22 -0300

    feat: creates logic to put the initial albums on the screen

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import searchAlbumsAPI from '../../services/searchAlbumsAPI'
 import initialAlbuns from '../../helpers/initialAlbums';
+import { Link } from 'react-router-dom';
 
 function Home() {
   const [searchInput, setSearchInput] = useState('');
@@ -10,29 +11,60 @@ function Home() {
     setSearchInput(value);
   };
 
-  return (
-    <div>
-      <h1 className="search-title">My tunes</h1>
-      <form className="search-form">
-        <h3>Search for your favorite artist album</h3>
-        <div>
-          <input
-            placeholder="type an artist name"
-            id="search-input"
-            name="search-input"
-            type="text"
-            value={ searchInput }
-            onChange={ handleSearch }
-          />
-        </div>
-        <button
-          className="search-button"
-          type="submit"
-        >
-          Search
-        </button>
+  useEffect(() => {
+    let arrayInitialAlbuns = [];
+    const fetchInitialAlbuns = () => {
+      initialAlbuns.forEach(async (album) => {
+        const response = await searchAlbumsAPI(album);
+        arrayInitialAlbuns = [...arrayInitialAlbuns, response[0]];
+        setGetAlbuns([...arrayInitialAlbuns]);
+      });
+    };
+    fetchInitialAlbuns();
+  }, []);
 
-      </form>
+  return (
+    <div className="page-search">
+      <div>
+        <h1 className="search-title">My tunes</h1>
+        <form className="search-form">
+          <h3>Search for your favorite artist album</h3>
+          <div>
+            <input
+              placeholder="type an artist name"
+              id="search-input"
+              name="search-input"
+              type="text"
+              value={ searchInput }
+              onChange={ handleSearch }
+            />
+          </div>
+          <button
+            className="search-button"
+            type="submit"
+          >
+            Search
+          </button>
+
+        </form>
+      </div>
+      <section className="albuns">
+        {getAlbuns.map((album) => (
+          <Link
+            className="links-search"
+            key={ album.collectionId }
+            data-testid={ `link-to-album-${album.collectionId}` }
+            to={ `/album/${album.collectionId}` }
+          >
+            <div>
+              <img src={ album.artworkUrl100 } alt="album.artistName" />
+              <p>{ album.artistName }</p>
+              <p>{ album.collectionName }</p>
+            </div>
+          </Link>
+        ))}
+      </section>
+
     </div>
   )
 }
