dae58cf31688e87bd051f82830e29c94f0e2b42d
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-21 08:52:45 -0300

    feat: create home page form state management

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home() {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleSearch = ({ target: { value } }) => {
+    setSearchInput(value);
+  };
+
   return (
     <div>
-        <h1 className="search-title">My tunes</h1>
-        <form className="search-form">
-          <h3>Search for your favorite artist album</h3>
-          <div>
-            <input
-              placeholder="type an artist name"
-              id="search-input"
-              name="search-input"
-              type="text"
-            />
-          </div>
-          <button
-            className="search-button"
-            type="submit"
-          >
-            Search
-          </button>
+      <h1 className="search-title">My tunes</h1>
+      <form className="search-form">
+        <h3>Search for your favorite artist album</h3>
+        <div>
+          <input
+            placeholder="type an artist name"
+            id="search-input"
+            name="search-input"
+            type="text"
+            value={ searchInput }
+            onChange={ handleSearch }
+          />
+        </div>
+        <button
+          className="search-button"
+          type="submit"
+        >
+          Search
+        </button>
 
-        </form>
-      </div>
+      </form>
+    </div>
   )
 }
 
