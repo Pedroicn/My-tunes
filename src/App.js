ea626e53ede90ec922a8f40202774912b4c1a290
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-21 11:23:36 -0300

    chore: change param getMusic from props to hook useParams

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Album from "./pages/album/Album";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
       <Route path="/home" element={ <Home /> } />
+      <Route path="/album/:id" element={ <Album /> } />
     </Routes>
   );
 }
--- src/pages/album/Album.jsx
+++ src/pages/album/Album.jsx
@@ -1,22 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 import getMusics from '../../services/musicsAPI';
 
-function Album(props) {
+function Album() {
   const [musics, setMusics] = useState([]);
   const [infoAlbum, setInfoAlbum] = useState([]);
+  const { id } = useParams();
 
   useEffect(() => {
-    const { match: { params: { id } } } = props;
     const fetchMusics = async () => {
       const apiMusics = await getMusics(id);
       setMusics(apiMusics);
       setInfoAlbum([apiMusics[0]]);
     };
     fetchMusics();
-  }, [props]);
+  }, [id]);
 
   return (
-    <div>Album</div>
+    <div className="page-album" data-testid="page-album">
+      <h1 className="search-title">My tunes - album</h1>
+      <section className="section-title-album">
+        {
+          infoAlbum.map((music, index) => (
+            <div className="actual-album" key={ index }>
+              <img src={ music.artworkUrl100 } alt="album" />
+              <p data-testid="artist-name">{ music.artistName }</p>
+              <p data-testid="album-name">{ music.collectionName }</p>
+            </div>
+          ))
+        }
+      </section>
+    </div>
   )
 }
 
