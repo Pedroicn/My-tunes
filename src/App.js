a3f16ec3407eb02976553a896fdd3eddb7b14b1e
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-24 11:04:41 -0300

    feat: create auth proveider

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Album from "./pages/album/Album";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import AuthProvider from "./context/AuthProvider";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Login /> } />
-      <Route path="/register" element={ <Register /> } />
-      <Route path="/home" element={ <Home /> } />
-      <Route path="/album/:id" element={ <Album /> } />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/register" element={ <Register /> } />
+        <Route path="/home" element={ <Home /> } />
+        <Route path="/album/:id" element={ <Album /> } />
+      </Routes>
+    </AuthProvider>
   );
 }
 
---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthContext from './AuthContext';
+
+function AuthProvider({ children }) {
+  return (
+    <AuthContext.Provider>
+      { children }
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
