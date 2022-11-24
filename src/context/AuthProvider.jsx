5dbe3b10f8d5db2ef3504f84f8ed6daed6decbb0
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-24 11:17:56 -0300

    feat: create feature to register user

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import AuthContext from './AuthContext';
+import auth from '../services/firebase';
+import { 
+  createUserWithEmailAndPassword,
+} from 'firebase/auth'
 
 function AuthProvider({ children }) {
+
+  const registerUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  const contextValue = { registerUser }
   return (
-    <AuthContext.Provider>
+    <AuthContext.Provider value={ contextValue }>
       { children }
     </AuthContext.Provider>
   )
